4f9a2047d70717bd6224995eab09cf11651a7af7
felipecordente <felipe_1998_@outlook.com>
2019-11-16 20:29:22 +0100

    Embotelladora terminada, con comentarios para entender bien la salida del programa

--- 04-practica-idm/src/embotelladora/Embotelladora.java
+++ 04-practica-idm/src/embotelladora/Embotelladora.java
@@ -9,16 +9,52 @@ public class Embotelladora {
 	//          se rellenan las grandes
 	// Eleva la excepción NoSolution si no es posible embotellar todo el lı́quido
 	public static int calculaBotellasPequenas(int pequenas, int grandes, int total) throws NoSolution {
-		System.out.println("pep");
-		return 0;
+		int num_litros_max = (pequenas * 1) + (grandes * 5);
+		int litros_restantes;
+		int num_g_out = 0;
+		int num_p_out = 0;
+		
+		if (pequenas < 0 || grandes < 0 || total < 0) {
+			throw new NoSolution("Parametros incorrectos");
+		}
+		if (num_litros_max < total) {
+			throw new NoSolution("Imposible embotellar todo el liquido, numero de botellas insuficientes");
+		}
+		
+		num_g_out = total / 5;
+		if (num_g_out <= grandes) {
+			grandes -= num_g_out;
+			if (grandes > 0) {
+				num_g_out += 1;
+				grandes -= 1;
+				litros_restantes = 0;
+				
+				System.out.println("Botellas grandes embotelladas= " + num_g_out);
+				System.out.println("Botellas grandes almacen= " + grandes);
+				System.out.println("Botellas pequeñas embotelladas= " + 0);
+				System.out.println("Botellas pequeñas almacen= " + pequenas);
+				System.out.println("Litros restantes por embotellar= " + litros_restantes);
+				return 0;
+			}
+		}else {
+			num_g_out = grandes;
+			grandes -= grandes;
+		}
+		litros_restantes = total - (num_g_out * 5);
+		
+		num_p_out = litros_restantes;
+		litros_restantes = 0;
+		
+		System.out.println("Botellas grandes embotelladas= " + num_g_out);
+		System.out.println("Botellas grandes almacen= " + grandes);
+		System.out.println("Botellas pequeñas embotelladas= " + num_p_out);
+		System.out.println("Botellas pequeñas almacen= " + pequenas);
+		System.out.println("Litros restantes por embotellar= " + litros_restantes);
+		
+		return num_p_out;
 	}
 	
-	public static void Main(String[] args) {
-		try {
-			calculaBotellasPequenas(1,1,1);
-		} catch (NoSolution e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	public static void main(String[] args) throws NoSolution {
+		calculaBotellasPequenas(3,3,13);
     }
 }
