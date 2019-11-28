53a160ae5668031bcc589b3e292d357ee2107eda
oscarescuderoarnanz <escuderoarnanzoscar@gmail.com>
2019-11-28 12:24:35 +0100

    Subo modificaciones

--- 04-practica-idm/src/descuento_blackfriday/DescuentoBlackFridayTest.java
+++ 04-practica-idm/src/descuento_blackfriday/DescuentoBlackFridayTest.java
@@ -2,6 +2,8 @@ package descuento_blackfriday;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import java.util.Calendar;
+import java.util.Date;
 
 public class DescuentoBlackFridayTest {
 
@@ -31,13 +33,18 @@ public class DescuentoBlackFridayTest {
 	}
 	
 	@Test
-	public void TestDescuentoNegativo() throws InvalidParameter{
-		try {
-			DescuentoBlackFriday.PrecioFinal(83.0, -3.0);
-		}catch (InvalidParameter e){
-			return;
-		}
-		fail("InvalidParameterException expected");
+	public void TestDescuentoBlackFriday() throws InvalidParameter{
+		Calendar c = Calendar.getInstance();
+	    Date fecha = new Date();
+	    int dia = c.get(Calendar.DATE); 
+	    int mes = c.get(Calendar.MONTH);
+	    
+	    System.out.println (fecha);
+	    if (dia == 29 && mes == 10) {
+			assertEquals("precio final con descuento: 72", 72, DescuentoBlackFriday.PrecioFinal(80.0, 10.0),0.1);
+	    }else {
+	    	assertEquals("precio final sin descuento: 80",80, DescuentoBlackFriday.PrecioFinal(80.0, 10.0),0.1);
+	    }
 	}
 
 }
